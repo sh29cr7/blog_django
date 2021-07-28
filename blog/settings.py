a8621a2fa421658784acbe5868d2dbde1166c5c3
sh29cr7 <shubham007j@gmail.com>
2021-07-28 20:02:13 +0530

    added the file upload functionality

--- blog/settings.py
+++ blog/settings.py
@@ -115,6 +115,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+#media configuration
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- blog_site/views.py
+++ blog_site/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from django.views import generic
 
 from .models import Post
+
+from django.conf import settings
+
+from django.core.files.storage import FileSystemStorage
+
 # Create your views here.
 
 class PostList(generic.ListView):
@@ -12,3 +17,14 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name='post_detail.html'
+
+class SimpleUpload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request,'/simple_upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request,'/simple_upload.html')
