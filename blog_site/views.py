b663b31fcc99dd0086b7c1cd5a03ec5b5db6e1b7
sh29cr7 <shubham007j@gmail.com>
2021-07-28 20:36:51 +0530

    added the file upload functionality

--- blog_site/views.py
+++ blog_site/views.py
@@ -18,13 +18,25 @@ class PostDetail(generic.DetailView):
     model = Post
     template_name='post_detail.html'
 
-def simple_upload(request):
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
-        return render(request,'/simple_upload.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
-    return render(request,'/simple_upload.html')
+#def simple_upload(request):
+  #  if request.method == 'POST' and request.FILES['myfile']:
+   #     myfile = request.FILES['myfile']
+   #     fs = FileSystemStorage()
+    #    filename = fs.save(myfile.name, myfile)
+     #   uploaded_file_url = fs.url(filename)
+      #  return render(request,'/simple_upload.html', {
+       #     'uploaded_file_url': uploaded_file_url
+        #})
+    #return render(request,'/simple_upload.html')
+
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, 'core/model_form_upload.html', {
+        'form': form
+    })
