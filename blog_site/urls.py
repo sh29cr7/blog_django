53562f68a014438aa940ac2ad3e5e4a079dab9b1
sh29cr7 <shubham007j@gmail.com>
2021-07-30 19:24:48 +0530

    added the download functionality

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 from blog_site.views import model_form_upload
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('',views.PostList.as_view(), name='home'),
@@ -9,3 +11,5 @@ urlpatterns = [
     path('<slug:slug>/',views.PostDetail.as_view(), name='post_detail'),
     
 ]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
